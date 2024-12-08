1192e0f9a33dcab5a38ac759422edbd3bf73bd77
李铭昕 <715557344@qq.com>
2024-12-08 18:31:48 +0800

    Added test cases

--- stringable_test.go
+++ stringable_test.go
@@ -8,4 +8,7 @@ import (
 func TestStringable_StrPad(t *testing.T) {
 	assert.Equal(t, "0001", StrPadLeft("1", rune('0'), 4))
 	assert.Equal(t, "1000", StrPadRight("1", rune('0'), 4))
+
+	assert.Equal(t, "0001", StrPadLeft("1", '0', 4))
+	assert.Equal(t, "1000", StrPadRight("1", '0', 4))
 }
