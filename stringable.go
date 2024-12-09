277c0369df8bf0c0d42371a3b9dc4627476fd379
李铭昕 <715557344@qq.com>
2024-12-09 19:50:17 +0800

    Added function `Md5()`

--- stringable.go
+++ stringable.go
@@ -1,6 +1,8 @@
 package go_stringable
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"errors"
 )
 
@@ -43,4 +45,10 @@ func StrPad(value string, pad rune, length int, padType int) (string, error) {
 	}
 
 	return string(paddedStr), nil
+}
+
+func Md5(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
--- stringable_test.go
+++ stringable_test.go
@@ -12,3 +12,7 @@ func TestStringable_StrPad(t *testing.T) {
 	assert.Equal(t, "0001", StrPadLeft("1", '0', 4))
 	assert.Equal(t, "1000", StrPadRight("1", '0', 4))
 }
+
+func TestStringable_Md5(t *testing.T) {
+	assert.Equal(t, "ebcd082fe9479bc278628297985ca2ba", Md5("Hyperf"))
+}
